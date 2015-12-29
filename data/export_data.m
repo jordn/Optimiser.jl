a605eef125658388b7bfad83f89354ef596d5044
Jordan Burgess <jordanburgess@gmail.com>
2015-12-29 18:38:53 +0000

    Check in data and instructions

---
+++ data/export_data.m
@@ -0,0 +1,8 @@
+load('data/4M17_assignment1.mat');
+
+csvwrite('data/A10.csv',A10);
+csvwrite('data/A100.csv',A100);
+csvwrite('data/A1000.csv',A1000);
+csvwrite('data/B10.csv',B10);
+csvwrite('data/B100.csv',B100);
+csvwrite('data/B1000.csv',B1000);
