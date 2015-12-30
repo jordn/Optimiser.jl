9f19cb54e59607b48fc8ea103d6c7bacfab74e61
Jordan Burgess <jordanburgess@gmail.com>
2015-12-30 00:26:05 +0000

    Tidy up

--- optimise.jl
+++ optimise.jl
@@ -6,8 +6,8 @@ const ϕ = 0.5 * (1.0 + sqrt(5.0))
 const global disp_progress = true
 f(x) = x.^4 .* cos(1./x) + 2x.^4
 g(x) = 4x.^3 .* cos(1./x) + x.^2 .* sin(1./x) + 8x.^3
-f(x) = x.^2-10x
-g(x) = 2x-10
+# f(x) = x.^2-10x
+# g(x) = 2x-10
 x = collect(-0.1:0.000001881:0.1)
 plot(x,f(x))
 
@@ -16,7 +16,7 @@ plot(x,f(x))
 
 function print_progress(xa, xb, xc, fa, fb, fc, evals)
   if disp_progress
-    const fmt = "{}: ({: 2.2f}, {: 2.2f}, {: 2.2f}) = ({: 2.3f}, {: 2.3f}, {: 2.3f})\n"
+    const fmt = "{}: ({: 2.4f}, {: 2.4f}, {: 2.4f}) = ({: 2.3f}, {: 2.3f}, {: 2.3f})\n"
     # if xa > xc
       # printfmt(fmt, evals, xc, xb, xa, fc, fb, fa)
       # @printf "%d: (% 2.2f, % 2.2f, % 2.2f) = (% 2.3f, % 2.3f, % 2.3f)\n" evals xc xb xa fc fb fa
@@ -73,13 +73,14 @@ end
 # xa, xb, xc, fa, fb, fc, evals =  bracket(f,1,-20,21)
 # xa, xb, xc, fa, fb, fc, evals =  bracket(f,1,6,111)
 
-function rebound(xa, xb, xc, x_new, fa, fb, fc, f_new)
+function rebracket(xa, xb, xc, x_new, fa, fb, fc, f_new)
   pts = [(xa, fa), (xb, fb), (xc, fc), (x_new, f_new)]
   x_order = sortperm(pts, by=pt->pt[1])
   ordered_pts = [pts[x_order[1]], pts[x_order[2]], pts[x_order[3]], pts[x_order[4]]]
   rank = sortperm(ordered_pts, by=pt->pt[2])
+  assert
   if rank[1] == 1 || rank[1] == 4
-    error("Fucked up the bracketing")
+    error("Minimum value not contained within the bracket.")
   end
   xa, fa = ordered_pts[rank[1]-1]
   xb, fb = ordered_pts[rank[1]]
@@ -88,70 +89,66 @@ function rebound(xa, xb, xc, x_new, fa, fb, fc, f_new)
 end
 
 
-function minimise(f::Function, x0, g=None; x_tolerance=0.01, max_evals=30)
+function minimise(f::Function, x0, g=None; x_tolerance=0.001,
+   grad_tolerance=1e-12, max_evals=100)
     tic();
-    const ϵ = 1e-10
-    const fmt = "{}: ({: 2.2f}, {: 2.2f}, {: 2.2f}) = ({: 2.3f}, {: 2.3f}, {: 2.3f})\n"
     evals = 0
 
     # fa < fc
     xa, xb, xc, fa, fb, fc, evals =  bracket(f, x0; max_evals=max_evals-evals)
     gradient = g(xb); evals += 1
-    while xc-xa > x_tolerance && abs(gradient) >= ϵ
+    pt = (xb, fb, gradient) # Current min point
+
+    stop_search(step=xc-xa) = step <= x_tolerance || abs(gradient) <= grad_tolerance
+    while !stop_search()
       println();
-      f_min = fb
       print_progress(xa, xb, xc, fa, fb, fc, evals)
+
+      """new_pt =(x_new, f_new, g_new)"""
+      function satisfies_wolfe(pt, new_pt, step_size, direction)
+        const wolfe_const1 = 1e-4
+        const wolfe_const2 = 0.9
+        sufficient_decrease = new_pt[2] <= pt[2] + wolfe_const1*step_size*direction*pt[3]
+        sufficient_curvature = new_pt[3] >= wolfe_const2*pt[3]*direction
+        return sufficient_decrease && sufficient_curvature
+      end
+
       direction = gradient <= 0 ? 1 : -1 # (p_k)
       if direction > 0
         step_size = (1-(1/ϕ))*(xc-xb)
       else
         step_size = (1-(1/ϕ))*(xb-xa)
       end
-      x_new = xb + step_size*direction
-      f_new = f(x_new); evals += 1
-      g_new = g(x_new); evals += 1
-      @printf "New pt: %0.3f => %0.3f   /%0.3f/\n" x_new f_new g_new
 
-      xa, xb, xc, fa, fb, fc = rebound(xa, xb, xc, x_new, fa, fb, fc, f_new)
-
-      function satisfies_wolfe()
-        const wolfe_const1 = 1e-4
-        const wolfe_const2 = 0.9
-        sufficient_decrease = f_new <= f_min + wolfe_const1 * step_size* gradient * direction
-        sufficient_curvature = g_new >= wolfe_const2 * gradient * direction
-        return sufficient_decrease && sufficient_curvature
-      end
-
-      # printfmt(fmt, evals, xa, xb, xc, fa, fb, fc)
-      print_progress(xa, xb, xc, fa, fb, fc, evals)
-      while !satisfies_wolfe() && step_size > x_tolerance && abs(g_new) >= ϵ
-        step_size = step_size*(1-(1/ϕ)) # Step length (α)
+      while true
         x_new = xb + step_size*direction
         f_new = f(x_new); evals += 1
         g_new = g(x_new); evals += 1
-        @printf "New pt: %0.3f => %0.3f   /%0.3f/\n" x_new f_new g_new
-        xa, xb, xc, fa, fb, fc = rebound(xa, xb, xc, x_new, fa, fb, fc, f_new)
-
+        xa, xb, xc, fa, fb, fc = rebracket(xa, xb, xc, x_new, fa, fb, fc, f_new)
         print_progress(xa, xb, xc, fa, fb, fc, evals)
+        new_pt = (x_new, f_new, g_new)
+        satisfies_wolfe(pt, new_pt, step_size, direction) && break
+        stop_search(step_size) && break
+        step_size = step_size*(1-(1/ϕ)) # Step length (α)
       end
-      println("leaving wolfe loop")
 
       if evals > max_evals
         error("Too many evaluations.")
       end
-      f_min = fb
       gradient = g(xb); evals += 1
+      pt = (xb, fb, gradient)
 
     end
     print_progress(xa, xb, xc, fa, fb, fc, evals)
     elapsed_time = toc();
-
-    return summarise(xa, xb, xc, fa, fb, fc, evals, elapsed_time)
+    return summarise(xa, xb, xc, fa, fb, fc, gradient, evals, elapsed_time)
 end
 
 
 """ Return a consistent data structure summarising the results. """
-function summarise(xa,xb,xc,fa,fb,fc,evals,elapsed_time)
+function summarise(xa,xb,xc,fa,fb,fc,gradient,evals,elapsed_time)
+  println(xa, " ", xb, " ", xc, " ", fa, " ", fb, " ", fc, " ", gradient,
+  " ", evals, " ", elapsed_time)
   return xb
 end
 
